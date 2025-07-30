9a4fc6763bbb2ba6661928b97d035d75e9a4cbc2
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-07-30 16:43:46 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose
+    .connect(process.env.MONGODB_URI )
+    .then(() => {
+        console.log('MongoDB connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
